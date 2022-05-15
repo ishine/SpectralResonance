255f8b1d0b301d380371ea0e107466cd7831b79c
DeepSeaMechanic <justme2020@outlook.com>
2022-05-14 22:56:58 -0400

    couatl

--- HackAudioPythonCode/Ch. 6 - Signal Gain and DC Offset/dbAmpChange.py
+++
@@ -1,7 +0,0 @@
-# DBAMPCHANGE
-# This function changes the amplitude of a input signal based on
-# a desired change relative to a decibel scale.
-
-def dbAmpChange(x, db_change):
-    scale = pow(10, db_change/20)  # Convert from decibel to linear
-    return scale * x  # Apply linear amplitude to signal
